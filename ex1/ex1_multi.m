c7295db20fa0e8bddc2cad7423323abecea0144c
kimegitee <kimegitee@gmail.com>
2017-06-08 22:05:38 +0700

    Use normal equation

--- ex1/ex1_multi.m
+++ ex1/ex1_multi.m
@@ -82,7 +82,7 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
+alpha = 1;
 num_iters = 400;
 
 % Init Theta and Run Gradient Descent 
@@ -105,7 +105,10 @@ fprintf('\n');
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
 price = 0; % You should change this
-
+X = [1650 3]
+X = (X - mu) ./ sigma
+X = [1 X]
+price = X * theta
 
 % ============================================================
 
@@ -149,7 +152,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = [1 1650 3] * theta; % You should change this
 
 
 % ============================================================
